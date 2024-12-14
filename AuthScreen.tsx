dcd85f7c4b8ea28db56450d55ec42f7bb9b7b7d7
Scott <105229291+ScottTan98@users.noreply.github.com>
2024-12-14 10:17:32 +0800

    React native create project first commit

---
+++ AuthScreen.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { View, Text, Button, Alert, StyleSheet } from 'react-native';
+import ReactNativeBiometrics from 'react-native-biometrics';
+
+const AuthScreen = ({navigation} : {navigation: any}) => {
+    console.log('navigation', navigation);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    const handleBiometricAuth = async () => {
+        const rnBiometrics = new ReactNativeBiometrics();
+
+        const { available, biometryType } = await rnBiometrics.isSensorAvailable();
+
+        if(available && (biometryType === 'FaceID' || biometryType === 'TouchID')) {
+            rnBiometrics
+                .simplePrompt({promptMessage: 'Authenticate with Face ID / Touch ID'})
+                .then(resultObject => {
+                    const { success } = resultObject;
+
+                    if (success) {
+                        Alert.alert('Authentication Successful');
+                        setIsAuthenticated(true);
+                        navigation.navigate('HomeScreen');
+                    } else {
+                        Alert.alert('Authentication Failed');
+                    }
+                })
+                .catch(() => {
+                    Alert.alert('Authentication Error');
+                });
+        } else {
+            Alert.alert('Biometric Authentication Not Available');
+        }
+    };
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>
+                {isAuthenticated
+                    ? 'Welcome to the Secure App!'
+                    : 'Please Authenticate to Continue'}
+            </Text>
+        <Button
+            title="Authenticate with Biometrics"
+            onPress={handleBiometricAuth}
+        />
+        </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    title: {
+      fontSize: 18,
+      fontWeight: 'bold',
+      marginBottom: 20,
+    },
+  });
+
+export default AuthScreen;
---
+++ HomeScreen.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to the Secure Transaction App!</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+  },
+  title: {
+      fontSize: 18,
+      fontWeight: 'bold',
+  },
+});
+
+export default HomeScreen;
