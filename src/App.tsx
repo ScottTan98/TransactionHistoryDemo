ee4464be622528bb9611121a4fd69cebcaea91f3
Scott <105229291+ScottTan98@users.noreply.github.com>
2024-12-16 10:17:37 +0800

    remove unuse component

--- src/App.tsx
+++ src/App.tsx
@@ -6,30 +6,12 @@
  */
 
 import React from 'react';
-import {
-  StatusBar,
-  useColorScheme,
-} from 'react-native';
-
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
 import Navigation from './navigation/navigation';
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    flex: 1,
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
 
   return (
     <>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
       <Navigation />
     </>
   );
